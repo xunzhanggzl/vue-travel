dd193b5e717016b3127f395d844cda8925873d6f
xunzhanggzl <1243286264@qq.com>
2020-02-08 11:26:37 +0800

    swiper

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import fastclick from 'fastclick' // 解决移动端300ms
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 import './assets/styles/reset.css' // 默认样式
 import './assets/styles/border.css' // 移动端1px边框
 import './assets/styles/iconfont.css' // 导入iconfont
+import 'swiper/dist/css/swiper.css'
 
 fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 
---
+++ src/views/home/components/Swiper.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="img-swiper" :src="item.imgUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        // some swiper options/callbacks
+        // 所有的参数同 swiper 官方 api 参数
+        // ...
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        effect: 'fade',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: 1,
+          imgUrl: 'https://ossweb-img.qq.com/upload/adw/image/20200207/a111592dbd4739f2c571d4ae4e19f247.jpeg'
+        },
+        {
+          id: 2,
+          imgUrl: 'https://ossweb-img.qq.com/upload/adw/image/20200201/ba5d308c7286765270a72066e42dadb2.jpg'
+        },
+        {
+          id: 3,
+          imgUrl: 'https://ossweb-img.qq.com/upload/adw/image/20200207/322246cf2cb877beb91cd2310f2fb366.jpeg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    width 100%
+    height 100%
+    // padding-bottom 41.25%
+    overflow hidden
+    .img-swiper
+      width 100%
+</style>
