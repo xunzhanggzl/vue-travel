87f7860ee3de76b026f58c1c12c0e1bc7414d4e5
xunzhanggzl <1243286264@qq.com>
2020-02-08 11:34:52 +0800

    padding-bottom 抖动

--- src/views/home/components/Swiper.vue
+++ src/views/home/components/Swiper.vue
@@ -51,8 +51,8 @@ export default {
     background #fff
   .wrapper
     width 100%
-    height 100%
-    // padding-bottom 41.25%
+    height 0
+    padding-bottom 41.25%
     overflow hidden
     .img-swiper
       width 100%
