f6cf61db13838f9899e9fb80e742648300ef1d53
xunzhanggzl <1243286264@qq.com>
2020-02-09 19:30:38 +0800

    兄弟组件传值 函数防抖

--- src/views/city/components/Alphabet.vue
+++ src/views/city/components/Alphabet.vue
@@ -1,7 +1,13 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) of cities" :key="key">
-      {{key}}
+    <li
+      @click="handleLetterClick" class="item" v-for="item of letters" :key="item"
+      :ref="item"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+    >
+      {{item}}
     </li>
   </ul>
 </template>
@@ -11,6 +17,52 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        // if (this.timer) {
+        //   clearTimeout(this.timer)
+        // }
+        // 很一般的防抖效果
+        clearTimeout(this.timer)
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 85
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 20)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
--- src/views/city/components/List.vue
+++ src/views/city/components/List.vue
@@ -21,7 +21,7 @@
           </div>
         </div>
       </div>
-      <div class="area" v-for="(item, key) of cities" :key="key">
+      <div class="area" v-for="(item, key) of cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
           <div
@@ -43,10 +43,21 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        console.log(this.$refs)
+        const el = this.$refs[this.letter][0]
+        console.log(el)
+        this.scroll.scrollToElement(el)
+      }
+    }
   }
 }
 </script>
